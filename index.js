5ba22e5f3a3766c49a77b6d35fd4a8572dea5581
TetianaSv <tiannadream@gmail.com>
2020-11-08 19:18:57 +0200

    made a timer for sale

---
+++ index.js
@@ -0,0 +1,61 @@
+
+const refs = {
+  days: document.querySelector('span[data-value="days"]'),
+  hours: document.querySelector('span[data-value="hours"]'),
+  mins: document.querySelector('span[data-value="mins"]'),
+  secs: document.querySelector('span[data-value="secs"]'),
+  timer: document.getElementById("timer-1")
+}
+
+class CountdownTimer {
+  constructor({ targetDate, selector }) {
+    this.targetDate = targetDate;
+    this.selector = selector;
+  }
+
+  
+  intervalId = setInterval(() => {
+  
+
+      const currentTime = Date.now();
+      const time = this.targetDate - currentTime;
+      
+    this.getTimeComponents(time)
+    this.endOfTimer(time)
+    }, 1000);
+  
+  
+  getTimeComponents(time) {
+const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+    // return { days, hours, mins, secs };
+
+ refs.days.textContent = `${days}`;
+ refs.hours.textContent = `${hours}`;
+ refs.mins.textContent = `${mins}`;
+ refs.secs.textContent = `${secs}`;
+    
+  function pad(value) {
+    return String(value).padStart(2, '0');
+    };
+  }
+  
+
+
+endOfTimer(time) {
+    if (time = 0) {
+      clearInterval(this.intervalId)
+    }
+  }
+  
+};
+
+
+
+new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('Nov 25, 2020 00:00:00'),
+});
+
